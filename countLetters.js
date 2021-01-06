b715cfc00ffd5a0f30c57c15967e7b157eaeb00c
munraj <munrajgrewal12@gmail.com>
2021-01-06 21:44:51 +0000

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterCount = {};
+  for (let letter of sentence){
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters("lighthouse in the house"));
