96ba014376861e26f96153ab3390cf6c5a00ffc4
munraj <munrajgrewal12@gmail.com>
2021-01-11 21:54:51 +0000

    seperating the tests to eqArraysTests

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let returnBool;
   if (array1.length === array2.length) {
@@ -13,7 +5,7 @@ const eqArrays = function(array1, array2) {
     for (let x = 0; x < array1.length; x++) {
       if (Array.isArray(array1[x]) && Array.isArray(array2[x])) {
         returnBool = eqArrays(array1[x], array2[x]);
-      } else{
+      } else {
         if (array1[x] === array2[x]) {
           returnBool = true;
         } else {
@@ -27,7 +19,4 @@ const eqArrays = function(array1, array2) {
   return returnBool;
 };
 
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+module.exports = eqArrays;
