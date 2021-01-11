52af82eb6bda96b6a075c4f3ab9dbcf5dc0c1b18
munraj <munrajgrewal12@gmail.com>
2021-01-11 22:01:22 +0000

    Seperating the tests to assertArraysEqualTests

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(array1, array2) {
+
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed`);
+  }
+};
+
+module.exports = assertArraysEqual;
